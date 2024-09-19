08281bbafa50bb6f62a78d1dbeabae45175bd712
Obaedur Rahman <arnobahemd12345@gmail.com>
2024-09-18 22:48:03 -0400

    More lessons

--- Rakib Vai/database.js
+++ Rakib Vai/database.js
@@ -1,2 +1,2 @@
-require("mongodb");
-const URI = "mongodb+srv://rahmo:BMH7ftE9r2kuCCWN@erpsyetem.4gb74.mongodb.net/"
+require('mongodb');
+const URI = 'mongodb+srv://rahmo:BMH7ftE9r2kuCCWN@erpsyetem.4gb74.mongodb.net/';
